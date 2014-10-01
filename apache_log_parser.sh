fd79553fca0d6b3f52707c556adb9c18ab069d4b
hiddenillusion <hiddenillusion@gmail.com>
2014-10-01 18:16:24 -0400

    adding apache log parser

---
+++ apache_log_parser.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+input_dir=$1
+output_dir="results.txt"
+total_files=`find $input_dir -type f | wc -l`
+
+count=1
+for file_path in `find $input_dir -type f`; do
+        fn=$(basename "$file_path")
+        #parent=$(echo $file_path | sed 's/\/mnt\/storage\/Cases\/mfr4-186\/tag001\///' | sed "s/$fn//")
+        echo "[*] [$count/$total_files]"
+        echo "[+] Processing: [$count/$total_files]"
+        echo "[-] Full Path : $file_path"
+        echo "[-] Parent folder : $parent"
+        echo "[-] Filename  : $fn"
+        cat $file_path | while read line; do
+                ip=`echo $line | awk '{print $2}'`
+                server=`echo $line | awk -F'"' '{print $12}'`
+                ts=`echo $line | awk -F'[' '{print $2}' | awk -F']' '{print $1}'`
+                pg=`echo $line | awk '{print $8}'`
+                echo $ts','$ip','$server','$pg
+        done
+        ((count++))
+done
+
